3b6e7266f722e70bb192889040a656941c6e6647
DESKTOP-76E8BOO\leeho <!Wlwl1070513>
2022-01-29 19:57:49 +0900

    Common 프로젝트를 라이브러리로 변경

--- SampleServer/SampleServer/main.cpp
+++ SampleServer/SampleServer/main.cpp
@@ -6,35 +6,35 @@
 int main()
 {
 	Iocp iocp(1);
-	/*Socket* listenSocket = new TcpSocket("127.0.0.1", 5555);
-	listenSocket->listen();*/
+	Socket* listenSocket = new TcpSocket("127.0.0.1", 5555);
+	listenSocket->listen();
 
-	//iocp.AddSocket(*listenSocket, nullptr);
-
-
-	//Socket* clientSocket = new TcpSocket();
-
-	//listenSocket->acceptOverlapped(*clientSocket);
-
-	//listenSocket->setIsOverlapping(true);
-
-	//while (true)
-	//{
-	//	// I/O 완료 이벤트가 있을 때까지 기다립니다.
-	//	IocpEvents readEvents;
-	//	iocp.getEvent(readEvents, 100);
-
-	//	// 받은 이벤트 각각을 처리합니다.
-	//	for (int index = 0; index < readEvents.m_eventCount; ++index)
-	//	{
-	//		OVERLAPPED_ENTRY& readEvent = readEvents.m_events[index];
+	iocp.AddSocket(*listenSocket, nullptr);
 
 
-	//	}
-	//}
+	Socket* clientSocket = new TcpSocket();
 
-	//delete(listenSocket);
-	//delete(clientSocket);
+	listenSocket->acceptOverlapped(*clientSocket);
+
+	listenSocket->setIsOverlapping(true);
+
+	while (true)
+	{
+		// I/O 완료 이벤트가 있을 때까지 기다립니다.
+		IocpEvents readEvents;
+		iocp.getEvent(readEvents, 100);
+
+		// 받은 이벤트 각각을 처리합니다.
+		for (int index = 0; index < readEvents.m_eventCount; ++index)
+		{
+			OVERLAPPED_ENTRY& readEvent = readEvents.m_events[index];
+
+
+		}
+	}
+
+	delete(listenSocket);
+	delete(clientSocket);
 
 	return 0;
 }
