e497519227cc9f9827132b8b553b6e3fa01ccc15
LAPTOP-NJ38A8SC\User <leehoseo@game.hoseo.edu>
2022-02-25 20:45:52 +0900

    서버 포트 설정

--- SampleServer/ServerLib/ServerIocp.cpp
+++ SampleServer/ServerLib/ServerIocp.cpp
@@ -21,7 +21,7 @@ void ServerIocp::init()
 	sockaddr_in server_addr;
 	ZeroMemory(&server_addr, sizeof(server_addr));
 	server_addr.sin_family = PF_INET;
-	server_addr.sin_port = htons(5555);
+	server_addr.sin_port = 15101;
 	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
 	bind(_mainSession, server_addr);
